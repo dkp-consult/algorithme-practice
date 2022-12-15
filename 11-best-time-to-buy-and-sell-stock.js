c0b9a074e3198b19fe39ded12b9484880972e0cf
dkp-consult <contact@dkp-consult.be>
2022-12-15 22:04:11 +0100

    thinking about eleven

---
+++ 11-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,11 @@
+// LeetCode challenge 11 : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+var maxProfit = function(prices) {
+
+};
+
+// classer les éléments par ordre croisant, choisir le plus faible et l'enrigistrer dans une variable et le plus haut pour le jour de vente
+// voir quand la variable apparait et acheter 
+// vérifier si l'index faible est avant l'index fort
+// si non, ne rien faire
+
